82378b079cca2f98b6bb67f60bcfc28cdafc35a9
Марат Тапканов <mi.tapkanov@stc.team>
2024-09-07 22:37:09 +0300

    Add unit tests and CI/CD workflow for GameHub.QuizMaster
    - Create new test project `GameHub.QuizMaster.Tests` with necessary references.
    - Add sample test class `AppConfigurationTest`.
    - Update CI workflow to trigger on changes to the `develop` branch.
    - Adjust CD workflow to build, test, and publish the application.
    - Fix project references and package versions in `GameHub.QuizMaster` and test project.

---
+++ tests/GameHub.QuizMaster.Tests/Infrastructure/Configuration/AppConfigurationTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace GameHub.QuizMaster.Tests.Infrastructure.Configuration;
+
+public class AppConfigurationTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
